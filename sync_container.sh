306b8a831886398a65843dcf4f502065644f2180
Mauro S. M. Rodrigues <maurosr@linux.vnet.ibm.com>
2022-09-23 18:24:47 -0500

    Rename container image for consistency
    Signed-off-by: Mauro S. M. Rodrigues <maurosr@linux.vnet.ibm.com>

--- sync_container.sh
+++ sync_container.sh
@@ -9,4 +9,4 @@ buildah config --workingdir /root $ctr
 buildah run $ctr -- /bin/sh -c 'dnf install -y iproute pip; pip install futures'
 buildah add $ctr ./server.py
 buildah config --cmd="python3 /root/server.py" $ctr
-buildah commit --rm $ctr maurosr/sync-server
+buildah commit --rm $ctr maurosr/sync-container
